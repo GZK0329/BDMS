c18971a862886a5a0cd0e26e79ade9a34766d417
gzk0329 <gzk0329@gmail.com>
2021-10-10 10:17:13 +0800

    init project

---
+++ bdms-master/src/main/java/com/gzk/bdms/BdmsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.gzk.bdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdmsMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdmsMasterApplication.class, args);
+    }
+
+}
---
+++ bdms-master/src/main/java/com/gzk/bdms/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.gzk.bdms.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Classname UserController
+ * @Description TODO
+ * @Date 2021/10/10 9:54
+ * @Created by GZK0329
+ */
+
+@Controller
+public class UserController {
+}
