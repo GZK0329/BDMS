8ecbe77e1e666be7449e8c11a71fba3c72c7d068
gzk0329 <gzk0329@gmail.com>
2021-12-14 23:44:26 +0800

    init

---
+++ src/test/java/com/gzk/bdms/BdmsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzk.bdms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdmsMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
